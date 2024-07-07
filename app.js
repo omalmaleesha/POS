970c86718c7cbfcdcf7f355af1e32e09d2e0dfc7
omalmaleesha <omalmaleesha03@gmail.com>
2024-07-07 17:39:33 +0530

    day2

--- app.js
+++ app.js
@@ -20,11 +20,39 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    //navigate to addCustomer interface
+    //navigate to addCustomer html
     const navigateAddCustomer = document.getElementById("navigationAddCustomer");
     if (navigateAddCustomer) {
         navigateAddCustomer.addEventListener("click", function(){
             window.location.href = "addCustomer.html";
         })
     }
+
+    //navigate delete Customer html
+    const navigateDeleteCustomer = document.getElementById("navigateDeleteCustomer");
+    if (navigateDeleteCustomer) {
+        navigateDeleteCustomer.addEventListener("click", function() {
+            window.location.href = "deleteCustomer.html"; 
+        });
+    }
+
+    //navigate to update customer html 
+    const navigateUpdateCustomer = document.getElementById("navigateUpdateCustomer");
+    if (navigateUpdateCustomer) {
+        navigateUpdateCustomer.addEventListener("click", function() {
+            window.location.href = "updateCustomer.html"; 
+        });
+    }
+
+    //navigate to search customer html
+    const navigateSearchCustomer = document.getElementById("navigateSearchCustomer");
+    if (navigateSearchCustomer) {
+        navigateSearchCustomer.addEventListener("click", function() {
+            window.location.href = "searchCustomer.html"; 
+        });
+    }
+
 });
+
+//Customer add delete update search
+
